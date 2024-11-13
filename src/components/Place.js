51da75de9918ff081bdb148d926d61894a4f6e38
Arnold Kiritu <arnoldkells4@gmail.com>
2024-11-13 23:46:24 +0300

    fixed different areas

---
+++ src/components/Place.js
@@ -0,0 +1,25 @@
+import "./PopularPlacesStyles.css";
+
+
+function Place({data}){
+    return(
+      <div>
+        <div className={data.customClass}>
+          <div className="description-text">
+            <h2>{data.heading}</h2>
+            <p>{data.text}
+          </p>
+          </div>
+          <div className="image">
+            <img src={data.image1} alt="" />
+            <img src={data.image2} alt="" />
+          </div>
+        </div>
+        <div>
+          {data.exText}
+        </div>
+      </div>
+    );
+}
+
+export default Place;
