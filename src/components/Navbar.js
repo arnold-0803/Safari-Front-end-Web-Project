ed02c8f6896e107c6c64d87251b012fa41257cf0
Arnold Kiritu <arnoldkells4@gmail.com>
2024-11-14 23:42:18 +0300

    replaced class component with function component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,34 @@
-import { Component } from "react";
 import "./NavbarStyles.css";
 import {Link} from "react-router-dom";
 import { MenuItems } from "./MenuItems";
+import { useState } from "react";
 
-class Navbar extends Component{
-  state = {clicked: false};
-  handleClick = () => {
-    this.setState({clicked: !this.state.clicked});
+const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
   }
-  render(){
-    return(
-      <nav className="navbar">
-        <Link to={"/"} className="navbar-logo"><h1>Safari</h1></Link>
-        <div className="menu-icons" onClick={this.handleClick}>
-          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return(
-              <li key={index}>
-                <Link to={item.url} className={item.cName}><i className={item.icon}></i>{item.title}</Link>
-              </li>
-            );
-          })}
-          <button>Sign Up</button>
-        </ul>
-      </nav>
-    );
-  }
+
+  return(
+    <nav className="navbar">
+      <Link to={"/"} className="navbar-logo"><h1>Safari</h1></Link>
+      <div className="menu-icons" onClick={handleToggle}>
+        <i className={toggle ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={toggle ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return(
+            <li key={index}>
+              <Link to={item.url} className={item.cName}><i className={item.icon}></i>{item.title}</Link>
+            </li>
+          );
+        })}
+        <button>Sign Up</button>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
