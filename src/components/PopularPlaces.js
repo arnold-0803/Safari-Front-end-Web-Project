c9dd13d12a3a62283517820d365e18c91a03276c
Arnold Kiritu <arnoldkells4@gmail.com>
2024-11-17 21:46:59 +0300

    updated some sections to give it a good look

--- src/components/PopularPlaces.js
+++ src/components/PopularPlaces.js
@@ -3,14 +3,14 @@ import Place from "./Place";
 
 const PopularPlaces = ({PopularPlaceData}) => {
   return (
-    <div className="places-wrapper">
+    <div className="segment">
       <h1 className="heading">
         Popular Places in Kenya
       </h1>
       <p className="subtitle">
         Tour gives you the opportunity to explore destinations of your dream
       </p>
-      <ul className="xl:px-[60px] max-[1281px]:px-[10px]">
+      <ul>
         {PopularPlaceData.length > 0 && PopularPlaceData.map(data => (
           <li key={data.id}>
             <Place 
--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
+
 const TripCard = (props) => {
   return (
     <div className="t-card">
       <div className="t-image">
         <img src={props.image} alt="" />
       </div>
-      <h4>{props.heading}</h4>
-      <p>{props.text}</p>
+      <div className="px-4 pb-4">
+        <h4 className="font-thin text-[#01959a] max-[767px]:text-[1.6rem] md:text-[1.8rem]">
+          {props.heading}
+        </h4>
+        <p>
+          {props.text.slice(0, 150)}...
+          <Link className="font-medium text-[#01959a]"
+            to={"/"}>
+            Read More
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
