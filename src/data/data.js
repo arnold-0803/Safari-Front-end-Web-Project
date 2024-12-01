97cbf4d7522dff18b7f3c73b98f31e838aded4aa
Arnold Kiritu <arnoldkells4@gmail.com>
2024-12-01 13:00:04 +0300

    changed some heading and added FAQ section

--- src/data/data.js
+++ src/data/data.js
@@ -30,3 +30,42 @@ export const MenuItems =[
     cName: "nav-link"
   }
 ];
+
+// FAQ data
+export const faqDatat = [
+  {
+    id: 0,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 1,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 2,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 3,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 4,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 5,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  },
+  {
+    id: 6,
+    question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+    answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit laudantium soluta non quis architecto velit reiciendis consequatur cum, iste, excepturi at repellat expedita accusamus, alias accusantium earum unde fugiat ea."
+  }
+]
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AboutContent from "../components/AboutContent";
 import Footer from "../components/Foofer";
 import Navbar from "../components/Navbar";
 import image from "../images/tour.avif";
 import MainFrame from "../components/MainFrame";
+import { faqDatat } from "../data/data";
 
 const About = ({scrollToTop}) => {
+
+  const [isOpen, setIsOpen] = useState(null);
+
+  const handleOpenAndClose = (id) => {
+    setIsOpen(isOpen === id ? null : id);
+  }
 
   useEffect(() => {
     scrollToTop();
@@ -28,6 +35,30 @@ const About = ({scrollToTop}) => {
           Know About Us
         </h1>
         <AboutContent/>
+      </div>
+      <div>
+        <h1 className="heading">
+          Frequently Asked Question
+        </h1>
+        <div className="lg:p-[4rem_6rem] max-[1023px]:p-[1.5rem_15px_4px]">
+          <ul className="list-decimal px-[20px]">
+            {faqDatat.map(item => (
+              <li className="pb-[10px] text-start marker:text-[#01959a] marker:font-medium md:text-[1.5rem]"
+                key={item.id}>
+                <h3 className="text-start md:text-[1.5rem] font-medium text-[#01959aaf] cursor-pointer inline"
+                  onClick={() => handleOpenAndClose(item.id)}>
+                  {item.question}
+                  <i className={isOpen === item.id ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down"}></i>
+                </h3>
+                {isOpen === item.id && 
+                  <p className="text-start pb-4 px-[15px] text-[1rem] text-[#01959a] border-b-[2px] border-[#01959a]">
+                  {item.answer}
+                </p>
+                }
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div>
         <Footer/>
